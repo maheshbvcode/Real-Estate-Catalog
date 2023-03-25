01cac1e287bc546d647e1ad7cf280727ecc2f987
Mahesh Bv <maheshbv2000@gmail.com>
2023-03-25 15:44:07 +0530

    added Routing

--- real-estate-frontend/src/App.js
+++ real-estate-frontend/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
-
+import SignIn from './pages/SignIn';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import SignUp from './pages/SignUp';
+import SideBar from './components/SideBar';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import BasicInfo from './components/BasicInfo';
+import Banner from './components/Banner';
+import PropDetails from './components/PropDetails';
+import GeneralInfo from './components/GeneralInfo';
+import LocationInfo from './components/LocationInfo';
 function App() {
   return (
- <div>
-          Hello team!...
+ <div className='app'>
+       <BrowserRouter>
+       <Routes>
+        <Route path='/' element={<SignIn/>}></Route>
+       
+       
+        <Route path='/signup' element={<SignUp/>}></Route>
+       
+       
+        <Route path='/home' element={<><SideBar/><Navbar/><Home/></>}></Route>
+       
+        <Route path='/properties' element={<><SideBar/><Navbar/><Banner/><PropDetails/></>}></Route>
+        <Route path='/basicinfo' element={<><SideBar/><Navbar/><Banner/><BasicInfo/></>}></Route>
+        {/* <Route path='/propertydetail' element={<><SideBar/><Navbar/><Banner/><PropertyDetail/></>}></Route> */}
+        <Route path='/general' element={<><SideBar/><Navbar/><Banner/><GeneralInfo/></>}></Route>
+        <Route path='/location' element={<><SideBar/><Navbar/><Banner/><LocationInfo/></>}></Route>
+       </Routes>
+       </BrowserRouter>   
  </div>
   );
 }
---
+++ real-estate-frontend/src/components/PropDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PropDetails = () => {
+  return (
+    <div>PropDetails</div>
+  )
+}
+
+export default PropDetails
