72d70a9d9d6e2e2d251c7e2528eaac9d31bb75a3
Mahesh Bv <maheshbv2000@gmail.com>
2023-03-25 20:47:07 +0530

    Added navbar and sideBar

--- real-estate-frontend/src/components/SearchBar.jsx
+++ real-estate-frontend/src/components/SearchBar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import '../styles/sidebar.css'
+import HomeIcon from '@mui/icons-material/Home';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import PlayForWorkIcon from '@mui/icons-material/PlayForWork';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 
-const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
+const SideBar = () => {
+    return (
+        <div className='main-sidebar'>
+            <div className='logo-side'>
+                <h1>Logo</h1>
+            </div>
+            <div className='logo-icon'>
+                <div className='side-flex' id='prop-icon'>
+                    <HomeIcon className='side-icon'/>
+                    <p>Property</p>
+                </div>
+                <div className='side-flex'>
+                    <NotificationsIcon className='side-icon'/>
+                    <p>Assistance</p>
+                </div>
+                <div className='side-flex'>
+                    <PlayForWorkIcon className='side-icon'/>
+                    <p>Recieved Interest</p>
+                </div>
+                <div className='side-flex-img'>
+                    <img id='side-img' src={require('../assets/path 255')} alt=""/>
+                    <p>Sent Interest</p>
+                </div>
+                <div className='side-flex'>
+                    <VisibilityIcon className='side-icon'/>
+                    <p>Property Views</p>
+                </div>
+                <div className='side-flex'>
+                    <LocalOfferIcon className='side-icon'/>
+                    <p>Traffic Plan</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default SearchBar
+
+export default SideBar
--- real-estate-frontend/src/components/SideBar.jsx
+++ real-estate-frontend/src/components/SideBar.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
-
+import React from "react";
+import { AiFillHome } from "react-icons/ai";
+import { MdNotifications } from "react-icons/md";
+import { GiReceiveMoney } from "react-icons/gi";
+import { MdUpload } from "react-icons/md";
+import { AiFillEye } from "react-icons/ai";
+import { BsFillTagFill } from "react-icons/bs";
+import '../styles/sidebar.css'
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <div className='main-sidebar'>
+            <div className='logo-side'>
+                <h1>Builders</h1>
+            </div>
+            <div className='logo-icon'>
+                <div className='side-flex' id='prop-icon'>
+                    <AiFillHome className='side-icon'/>
+                    <p>Property</p>
+                </div>
+                <div className='side-flex'>
+                    <MdNotifications className='side-icon'/>
+                    <p>Assistance</p>
+                </div>
+                <div className='side-flex'>
+                    <GiReceiveMoney className='side-icon'/>
+                    <p>Recieved Interest</p>
+                </div>
+                <div className='side-flex-img'>
+                    <MdUpload className='side-icon'/>
+                    <p>Sent Interest</p>
+                </div>
+                <div className='side-flex'>
+                    <AiFillEye className='side-icon'/>
+                    <p>Property Views</p>
+                </div>
+                <div className='side-flex'>
+                    <BsFillTagFill className='side-icon'/>
+                    <p>Traffic Plan</p>
+                </div>
+            </div>
+        </div>
   )
 }
 
